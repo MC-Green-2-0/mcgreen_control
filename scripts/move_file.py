f997e3a33f45593cb58168a99a6f2f0a66bb244e
Raj Joshi <geeky2307@gmail.com>
2020-08-15 10:11:32 -0400

    modified move_file script

--- scripts/move_file.py
+++ scripts/move_file.py
@@ -1,8 +1,6 @@
 from paramiko import SSHClient
 from scp import SCPClient
 
-cmdu = "rm -r ~/upper_ws/src/mcgreen_control/"
-cmdl = "rm -r ~/lower_ws/src/mcgreen_control/"
 dir = "/home/mcgreen/mcgreen2_ws/src/mcgreen_control"
 
 host = "upper_pi"
@@ -10,12 +8,13 @@ port = 22
 username = "upperpi"
 password = "mcgreen"
 rm_dir = "~/upper_ws/src"
+cmd = "rm -r ~/upper_ws/src/mcgreen_control/"
 
 ssh = SSHClient()
 ssh.load_system_host_keys()
 ssh.connect(host, port, username, password)
 
-(ssh_stdin, ssh_stdout, ssh_stderr) = ssh.exec_command(cmdu)
+(ssh_stdin, ssh_stdout, ssh_stderr) = ssh.exec_command(cmd)
 ssh.close()
 del ssh_stdin, ssh_stdout, ssh_stderr
 
@@ -34,12 +33,13 @@ port = 22
 username = "lowerpi"
 password = "mcgreen"
 rm_dir = "~/lower_ws/src"
+cmd = "rm -r ~/lower_ws/src/mcgreen_control/"
 
 ssh = SSHClient()
 ssh.load_system_host_keys()
 ssh.connect(host, port, username, password)
 
-(ssh_stdin, ssh_stdout, ssh_stderr) = ssh.exec_command(cmdl)
+(ssh_stdin, ssh_stdout, ssh_stderr) = ssh.exec_command(cmd)
 ssh.close()
 del ssh_stdin, ssh_stdout, ssh_stderr
 

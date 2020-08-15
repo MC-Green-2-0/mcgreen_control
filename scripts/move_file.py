a824f719ef086c7324c6c3781b66c72ba323b43d
Aditya Vidyadharan <aditya@vidyadharan.com>
2020-08-14 20:12:37 -0400

    added file transfer script

--- scripts/move_file.py
+++ scripts/move_file.py
@@ -1,11 +1,22 @@
 from paramiko import SSHClient #pip install these
 from scp import SCPClient
+
+cmdu = "rm -r ~/upper_ws/src/mcgreen_control/"
+cmdl = "rm -r ~/lower_ws/src/mcgreen_control/"
+
+
 dir = "/home/mcgreen/mcgreen2_ws/src/mcgreen_control"#put in directory for mcgreen folder on nuc
 host = "upper_pi"
 port = 22
 username = "upperpi"
 password = "mcgreen" # not sure about this
-rm_dir = "~/upper_ws/src/mcgreen_control"#put in directory for mcgreen folder on upperpi
+rm_dir = "~/upper_ws/src"#put in directory for mcgreen folder on upperpi
+ssh = SSHClient()
+ssh.load_system_host_keys()
+ssh.connect(host, port, username, password)
+(ssh_stdin, ssh_stdout, ssh_stderr) = ssh.exec_command(cmdu)
+ssh.close()
+del ssh_stdin, ssh_stdout, ssh_stderr
 ssh = SSHClient()
 ssh.load_system_host_keys()
 ssh.connect(host, port, username, password)
@@ -15,14 +26,22 @@ scp.put(dir, recursive = True, remote_path = rm_dir)
 # '/home/user/dump' remote directory
 #scp.put('test', recursive=True, remote_path='/home/user/dump')
 scp.close()
+ssh.close()
 host = "lower_pi"
 port = 22
 username = "lowerpi"
 password = "mcgreen" # not sure about this
-rm_dir = "~/lower_ws/src/mcgreen_control" #put in directory for mcgreen folder on lowerpi
+rm_dir = "~/lower_ws/src" #put in directory for mcgreen folder on lowerpi
+ssh = SSHClient()
+ssh.load_system_host_keys()
+ssh.connect(host, port, username, password)
+(ssh_stdin, ssh_stdout, ssh_stderr) = ssh.exec_command(cmdl)
+ssh.close()
+del ssh_stdin, ssh_stdout, ssh_stderr
 ssh = SSHClient()
 ssh.load_system_host_keys()
 ssh.connect(host, port, username, password)
 scp = SCPClient(ssh.get_transport())
 scp.put(dir, recursive = True, remote_path = rm_dir)
 scp.close()
+ssh.close()

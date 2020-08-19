a69e376031a1abad49cd79ffa6b86efb5d5eb7cd
Raj Joshi <geeky2307@gmail.com>
2020-08-18 20:12:05 -0400

    movement_output works

--- src/NUC/movement_output.py
+++ src/NUC/movement_output.py
@@ -15,18 +15,18 @@ class Movement_Output:
     FACE_IN="/game_face"
     FACE_OUT="/facial_expression"
     threshold = rospy.get_param("Sensors/threshold_ultra")
-
     def __init__(self):
         self.upper_safe = Array()
         self.lower_safe = Array()
         self.feedback = Bool()
+        self.ultrasonic = [80]*2
         self.feedback.data = True
         self.safe = True
-        self.safety_clear = 1000
-        self.upper = [1500]*2 + [90] * 2
-        self.lower = [1500]*4
+        self.upper = [1500] * 2 + [90] * 2
+        self.lower = [1500] * 4
         self.face = Int16()
         self.face.data = 4
+        self.safety_toggle = False
         self.up_sub = rospy.Subscriber(self.UPPER_IN, Array, self.up_update)
         self.low_sub = rospy.Subscriber(self.LOWER_IN, Array, self.low_update)
         self.sensor_sub = rospy.Subscriber(self.US_TOPIC, Sensor, self.sensor_update)
@@ -37,7 +37,7 @@ class Movement_Output:
         self.lower_pub = rospy.Publisher(self.LOWER_OUT, Array, queue_size = 1)
         self.safety_feedback_pub = rospy.Publisher(self.FEEDBACK_TOPIC, Bool, queue_size = 1)
         self.safety_feedback_pub.publish(self.feedback)
-        self.us=[50]*4
+
 
     def up_update(self,data):
         self.upper = list(data.arr)
@@ -49,44 +49,49 @@ class Movement_Output:
         self.face = data
 
     def sensor_update(self, data):
-        #Need to add code to deal with bad data
-        self.us = data.ultrasonic
+        self.ultrasonic = list(data.ultrasonic)
 
     def toggle_update(self, data):
         try:
-            self.safety_clear = data.ts[5]
+            if(data.ts[5] == 2000):
+                self.safety_toggle = True
+            else:
+                self.safety_toggle = False
         except IndexError:
-            self.safety_clear = 1000
+            self.safety_toggle = False
 
     def update(self):
         # upper_safe = [arm_left, arm_right, head_x, head_y]
-        if not all(item > self.threshold for item in self.us):
+        if (self.ultrasonic[0] < self.threshold or self.ultrasonic[1] < self.threshold):
             self.lower_safe.arr = [1500]*4
-            self.upper_safe.arr[:2] = [1500]*2
+            self.upper_safe.arr = [0]*2 + [90]*2
             self.safe = False
+
+        if (self.safety_toggle == True):
+            self.safe = True
+
         if self.safe == False:
             warn_face = Int16()
             warn_face.data = 0
             self.face_pub.publish(warn_face)
-            if self.safety_clear == 2000:
-                self.safe = True
+            self.upper_pub.publish(self.upper_safe)
+            self.lower_pub.publish(self.lower_safe)
+
         if self.safe == True:
             self.lower_safe.arr = self.lower
             self.upper_safe.arr = self.upper
             self.face_pub.publish(self.face)
+            self.upper_pub.publish(self.upper_safe)
+            self.lower_pub.publish(self.lower_safe)
+
         if self.safe != self.feedback.data:
             self.feedback.data = self.safe
             self.safety_feedback_pub.publish(self.feedback)
-        self.upper_safe.arr[2:] = self.upper[2:]
-        self.upper_pub.publish(self.upper_safe)
-        self.lower_pub.publish(self.lower_safe)
-
 
 if __name__ == "__main__":
-    rospy.init_node("safety_cutoff")
+    rospy.init_node("Movement_Output")
     args = {"rate": rospy.get_param("~rate")}
     safe = Movement_Output()
-    #rospy.spin()
     r = rospy.Rate(args["rate"])
     while not rospy.is_shutdown():
         safe.update()

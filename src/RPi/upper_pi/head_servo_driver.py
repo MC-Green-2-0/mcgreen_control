cf33cf69f875a084bf4078b0ac53af8c0e4c9376
Raj Joshi <geeky2307@gmail.com>
2020-08-19 17:45:19 -0400

    Head Sevo preliminary work completed+

--- src/RPi/upper_pi/head_servo_driver.py
+++ src/RPi/upper_pi/head_servo_driver.py
@@ -15,31 +15,22 @@ class Head_Servo_Driver:
         self.tog_sub = rospy.Subscriber(self.SERVO_TOPIC, Array, self.servo_callback)
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)#set pin numbering system
-        GPIO.setup(11,GPIO.OUT)
-        GPIO.setup(13, GPIO.OUT)
-        self.vertical_controller = GPIO.PWM(11,50)		#create PWM instance with frequency
-        self.horizontal_controller = GPIO.PWM(13, 50)
-        self.vertical_controller.start(50)				#start PWM of required Duty Cycle
-        self.horizontal_controller.start(50)
-	for x in range(0,90):
-		self.vertical_controller.ChangeDutyCycle(2+float(x*2)/18)
-		#time.sleep(0.1)
-		#self.vertical_controller.ChangeDutyCycle(0)
+        GPIO.setup(7,GPIO.OUT)
+        GPIO.setup(19, GPIO.OUT)
+        self.vertical_controller = GPIO.PWM(7,50)		#create PWM instance with frequency
+        self.horizontal_controller = GPIO.PWM(19, 50)
+        self.vertical_controller.start(0)				#start PWM of required Duty Cycle
+        self.horizontal_controller.start(0)
+
     def servo_callback(self, data):
-        print("raw: " + str(data.arr))
-	    horizontal_angle = 2 + float(180 - data.arr[2])/18
-        vertical_angle=2+float(data.arr[3])/18
-	    self.vertical_controller.ChangeDutyCycle(vertical_angle)
+        horizontal_angle = int(2 + (data.arr[2]/18))
+        vertical_angle = int(2 + (data.arr[3]/18))
+        self.vertical_controller.ChangeDutyCycle(vertical_angle)
         self.horizontal_controller.ChangeDutyCycle(horizontal_angle)
-	#time.sleep(0.5)
-	#self.vertical_controller.ChangeDutyCycle(0)
-	#self.horizontal_controller.ChangeDutyCycle(0)
-	print(vertical_angle, horizontal_angle)
 
     def clean(self):
         self.vertical_controller.ChangeDutyCycle(7)
         self.horizontal_controller.ChangeDutyCycle(7)
-        print("base position")
 
 if __name__ == "__main__":
     try:
@@ -48,6 +39,6 @@ if __name__ == "__main__":
         rospy.spin()
         rospy.on_shutdown(controller.clean)
     except KeyboardInterrupt:
-		pass
-	except rospy.ROSInterruptException:
-		pass
+        pass
+    except rospy.ROSInterruptException:
+        pass
